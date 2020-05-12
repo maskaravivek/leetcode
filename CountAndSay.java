a0ab864e69903f40c94381f48abe53ac3db03f58
Vivek Maskara <maskaravivek@gmail.com>
2020-05-12 13:08:47 -0700

    With count and say

---
+++ CountAndSay.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CountAndSay {
+
+    public static void main(final String[] args) throws java.lang.Exception {
+        System.out.println(countAndSay(5));
+    }
+
+    public static String countAndSay(int n) {
+        String num = "1";
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 2; i <= n; i++) {
+            char c = num.charAt(0);
+            int count = 1;
+            String subStr = "";
+            for (int j = 1; j < num.length(); j++) {
+                if (c == num.charAt(j)) {
+                    count++;
+                } else {
+                    subStr += count + "" + c;
+                    count = 1;
+                    c = num.charAt(j);
+                }
+            }
+            subStr += count + "" + c;
+            num = subStr;
+        }
+        return num;
+    }
+}
