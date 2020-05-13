027f81d091659e811ae44ef66529c0a24fc798a9
Vivek Maskara <maskaravivek@gmail.com>
2020-05-13 15:51:44 -0700

    length of last word

---
+++ LengthOfLastWord.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/length-of-last-word
+
+class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] split = s.split(" ");
+        int len = split.length;
+        if(len == 0) {
+            return 0;
+        } else {
+            return split[len-1].length();
+        }
+    }
+}
