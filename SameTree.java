f21c2d30a17b1afd4fb1874c1f2c84a509c2479e
Vivek Maskara <maskaravivek@gmail.com>
2020-05-14 19:32:22 -0700

    With same tree

---
+++ SameTree.java
@@ -0,0 +1,12 @@
+class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if((p == null && q!=null) || (p != null && q==null)) {
+            return false;
+        } else if(p == null && q == null) {
+            return true;
+        } else {
+            return p.val == q.val && isSameTree(p.right, q.right) 
+            && isSameTree(p.left, q.left); 
+        }  
+    }
+}
--- TreeNode.java
+++ TreeNode.java
@@ -1,5 +1,4 @@
 
-
 /**
  * Definition for a binary tree node. public class TreeNode { int val; TreeNode
  * left; TreeNode right; TreeNode(int x) { val = x; } }
@@ -12,5 +11,11 @@ public class TreeNode {
 
     TreeNode(int x) {
         val = x;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
