a58386e8c1f896c87c54cec3ba1ffc0138fd186e
Vivek Maskara <maskaravivek@gmail.com>
2020-05-15 10:51:11 -0700

    With flipping an image

---
+++ FlippingAnImage.java
@@ -0,0 +1,13 @@
+public class FlippingAnImage {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int[][] flipped = new int[A[0].length][A.length];
+        
+        int len = A[0].length;
+        for(int i=0; i<A.length; i++) {
+            for(int j=len -1; j>=0; j--) {
+                flipped[i][len -1 - j] = A[i][j] == 1? 0: 1;
+            }
+        }
+        return flipped;
+    }
+}
