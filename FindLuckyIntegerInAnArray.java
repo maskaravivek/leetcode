2439f151ed3658b6c5fb676bf4724bd035bc77ba
Vivek Maskara <maskaravivek@gmail.com>
2020-05-15 10:31:56 -0700

    With find lucky integer in an array

---
+++ FindLuckyIntegerInAnArray.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class FindLuckyIntegerInAnArray {
+    public int findLucky(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i< arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        int largest = -1;
+        for(int key: map.keySet()) {
+            if(map.get(key) == key && key > largest) {
+                largest = key;
+            }
+        }
+        return largest;
+    }
+}
