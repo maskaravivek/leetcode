3d186e3b1574f7ac4f4bf675bfbe5b33d3a291b7
Vivek Maskara <maskaravivek@gmail.com>
2020-05-17 14:00:24 -0700

    With binary tree post order traversal

---
+++ BinaryTreePostOrderTraversal.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BinaryTreePostOrderTraversal {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> tree = new ArrayList();
+        postOrder(root, tree);
+        return tree;
+    }
+
+    public void postOrder(TreeNode root, List<Integer> tree) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left, tree);
+        postOrder(root.right, tree);
+        tree.add(root.val);
+    }
+}
