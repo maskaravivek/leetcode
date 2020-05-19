d63d2f629d2fffb5adee40232e60010a4b1669d7
Vivek Maskara <maskaravivek@gmail.com>
2020-05-19 16:27:27 -0700

    Max depth of a nary tree

---
+++ MaximumDepthOfANAryTree.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class MaximumDepthOfANAryTree {
+    public int maxDepth(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<List<Node>> queue = new LinkedList<>();
+        queue.add(Arrays.asList(root));
+
+        while (!queue.isEmpty()) {
+            List<Node> level = queue.poll();
+            List<Integer> numbers = new ArrayList<>();
+            List<Node> nodes = new ArrayList<>();
+            for (Node node : level) {
+                numbers.add(node.val);
+                for(Node childNode: node.children) {
+                    nodes.add(childNode);
+                }
+            }
+            result.add(numbers);
+            if (nodes.size() > 0) {
+                queue.add(nodes);
+            }
+        }
+        return result.size();
+    }
+}
