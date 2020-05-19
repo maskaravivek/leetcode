56f26f57a4dd6fe48531bbc17d0d41c7b1b99f95
Vivek Maskara <maskaravivek@gmail.com>
2020-05-19 01:08:53 -0700

    with in order traversal II

--- BinaryTreeLevelOrderTraversalII.java
+++ BinaryTreeLevelOrderTraversalII.java
@@ -1,28 +1,27 @@
-// https://leetcode.com/problems/average-of-levels-in-binary-tree/
+// https://leetcode.com/problems/binary-tree-level-order-traversal-ii
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class AverageOfLevelsInABinaryTree {
-    public List<Double> averageOfLevels(TreeNode root) {
+class BinaryTreeLevelOrderTraversalII {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
         if (root == null) {
             return new ArrayList<>();
         }
-        List<Double> result = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Queue<List<TreeNode>> queue = new LinkedList<>();
         queue.add(Arrays.asList(root));
 
         while (!queue.isEmpty()) {
             List<TreeNode> level = queue.poll();
-            double sum = 0.0;
-            int count = 0;
+            List<Integer> numbers = new ArrayList<>();
             List<TreeNode> nodes = new ArrayList<>();
             for (TreeNode node : level) {
-                sum += node.val;
-                count++;
+                numbers.add(node.val);
                 if (node.left != null) {
                     nodes.add(node.left);
                 }
@@ -30,15 +29,12 @@ public class AverageOfLevelsInABinaryTree {
                     nodes.add(node.right);
                 }
             }
-            double avg = 0.0;
-            if(count > 0) {
-                avg = (double) sum / count;
-            }
-            result.add(avg);
+            result.add(numbers);
             if (nodes.size() > 0) {
                 queue.add(nodes);
             }
         }
+        Collections.reverse(result);
         return result;
     }
 }
