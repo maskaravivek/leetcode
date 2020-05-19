84c0071f3dcc23b122c3ed9ba5175e315897a49e
Vivek Maskara <maskaravivek@gmail.com>
2020-05-19 01:22:22 -0700

    with zig zag order traversal

--- BinaryTreeZigZagOrderTraversal.java
+++ BinaryTreeZigZagOrderTraversal.java
@@ -1,39 +1,44 @@
-// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class MinimumDepthOfABinaryTree {
-    public int minDepth(TreeNode root) {
+class BinaryTreeZigZagOrderTraversal {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
         if (root == null) {
-            return 0;
+            return new ArrayList<>();
         }
+        List<List<Integer>> result = new ArrayList<>();
         Queue<List<TreeNode>> queue = new LinkedList<>();
-        int depth = 1;
         queue.add(Arrays.asList(root));
-
+        int levelCount = 1;
         while (!queue.isEmpty()) {
             List<TreeNode> level = queue.poll();
+            List<Integer> numbers = new ArrayList<>();
             List<TreeNode> nodes = new ArrayList<>();
             for (TreeNode node : level) {
-                if(node.left == null && node.right == null) {
-                    return depth;
-                }
+                numbers.add(node.val);
                 if (node.left != null) {
                     nodes.add(node.left);
-                } 
+                }
                 if (node.right != null) {
                     nodes.add(node.right);
-                } 
+                }
             }
+            if(levelCount%2 == 0) {
+                Collections.reverse(numbers);
+            }
+            result.add(numbers);
+
+            levelCount++;
             if (nodes.size() > 0) {
                 queue.add(nodes);
-                depth++;
             }
         }
-        return depth;
+        return result;
     }
 }
