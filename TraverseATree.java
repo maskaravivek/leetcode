2e835c19fddcb9c90c78bec361fddbfe30405f91
Vivek Maskara <maskaravivek@gmail.com>
2020-05-18 23:23:18 -0700

    with binary tree level order traversal

---
+++ TraverseATree.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TraverseATree {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<List<TreeNode>> queue = new LinkedList<>();
+        queue.add(Arrays.asList(root));
+
+        while (!queue.isEmpty()) {
+            List<TreeNode> level = queue.poll();
+            List<Integer> numbers = new ArrayList<>();
+            List<TreeNode> nodes = new ArrayList<>();
+            for (TreeNode node : level) {
+                numbers.add(node.val);
+                if (node.left != null) {
+                    nodes.add(node.left);
+                }
+                if (node.right != null) {
+                    nodes.add(node.right);
+                }
+            }
+            result.add(numbers);
+            if (nodes.size() > 0) {
+                queue.add(nodes);
+            }
+        }
+        return result;
+    }
+}
