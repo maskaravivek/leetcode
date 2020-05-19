ac45b38ff792bf32e3e89a8dc1c4f16714ec52f5
Vivek Maskara <maskaravivek@gmail.com>
2020-05-19 01:16:56 -0700

    with min depth of a binary tree

---
+++ MinimumDepthOfABinaryTree.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class MinimumDepthOfABinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<List<TreeNode>> queue = new LinkedList<>();
+        int depth = 1;
+        queue.add(Arrays.asList(root));
+
+        while (!queue.isEmpty()) {
+            List<TreeNode> level = queue.poll();
+            List<TreeNode> nodes = new ArrayList<>();
+            for (TreeNode node : level) {
+                if(node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    nodes.add(node.left);
+                } 
+                if (node.right != null) {
+                    nodes.add(node.right);
+                } 
+            }
+            if (nodes.size() > 0) {
+                queue.add(nodes);
+                depth++;
+            }
+        }
+        return depth;
+    }
+}
