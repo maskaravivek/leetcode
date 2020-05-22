90df64370b3938af48520107ae4bbadafe4e7955
Vivek Maskara <maskaravivek@gmail.com>
2020-05-22 12:41:21 -0700

    Minimum cost climbing stairs

---
+++ MinCostClimbingStairs.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs
+
+class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int dp[]= new int[n];
+        
+        for(int i=0; i< n; i++) {
+            if(i<=1) {
+                dp[i] = cost[i];
+            } else {
+                dp[i] = Integer.MAX_VALUE;
+            }
+        }
+        
+        for(int i=2; i< n; i++) {
+            for(int j=i-2; j< i; j++) {
+                dp[i] = min(dp[i], dp[j] + cost[i]);   
+            }
+        }
+        return min(dp[n-1], dp[n-2]);
+    }
+    
+    public int min(int a, int b) {
+        return a>b?b:a;
+    }
+}
