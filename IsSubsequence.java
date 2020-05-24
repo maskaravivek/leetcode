58bafb9e11c4127a73830ec7a672fcdd63ed36a6
Vivek Maskara <maskaravivek@gmail.com>
2020-05-23 23:20:19 -0700

    With is subsequence

---
+++ IsSubsequence.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/is-subsequence
+
+class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() == 0) {
+            return true;
+        } 
+        int sidx = 0;
+        char schar = s.charAt(0);
+        for(int i = 0; i<t.length();i++) {
+            char c = t.charAt(i);
+            if(schar == c) {
+                if(sidx == (s.length() - 1)) {
+                    return true;
+                } else {
+                    sidx++;
+                    schar = s.charAt(sidx);
+                }
+            }
+        }
+        return false;
+    }
+}
