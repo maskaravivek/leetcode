11dfc2ffca9d9589eb803590aa69293c555fa0be
Vivek Maskara <maskaravivek@gmail.com>
2020-05-24 20:46:44 -0700

    with replace element with greatest element on right side

---
+++ ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
+
+class ReplaceElementsWithGreatestElementOnRightSide {
+    public int[] replaceElements(int[] arr) {
+        int len = arr.length;
+        int max = arr[len-1];
+        arr[len-1] = -1;
+        for(int i=len-2; i>=0; i--) {
+            int newMax = arr[i] > max? arr[i]: max;
+            arr[i] = max;
+            max = newMax;
+        }
+        return arr;
+    }
+}
