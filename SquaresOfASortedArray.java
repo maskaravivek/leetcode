df030868114058356a1e659c294802aeef1334cd
Vivek Maskara <maskaravivek@gmail.com>
2020-05-25 19:07:58 -0700

    with o(n) solution for squares of a sorted array

--- SquaresOfASortedArray.java
+++ SquaresOfASortedArray.java
@@ -2,12 +2,36 @@ import java.util.Arrays;
 
 class SquaresOfASortedArray {
     public int[] sortedSquares(int[] A) {
-        int arrayLength = A.length;
-        int arr[] = new int[arrayLength];
-        for(int i=0; i< A.length; i++) {
-            arr[i] = A[i] * A[i];
+        int idx = 0;
+        for(int i=0; i<A.length; i++) {
+            if(A[idx] <=0) {
+                idx++;
+            }
         }
-        Arrays.sort(arr);
-        return arr;
+        int ans[] = new int[A.length];
+        int negativeIdx=idx-1;
+        int positiveIdx = idx;
+        int ansIdx = 0;
+        
+        while(negativeIdx >=0 && positiveIdx<A.length) {
+            if(A[positiveIdx] > Math.abs(A[negativeIdx])) {
+                ans[ansIdx++] = A[negativeIdx] * A[negativeIdx];
+                negativeIdx--;
+            } else {
+                ans[ansIdx++] = A[positiveIdx] * A[positiveIdx];
+                positiveIdx++;
+            }
+        }
+        
+        while(negativeIdx>=0) {
+            ans[ansIdx++] = A[negativeIdx] * A[negativeIdx];
+            negativeIdx--;
+        }
+        
+        while(positiveIdx<A.length) {
+            ans[ansIdx++] = A[positiveIdx] * A[positiveIdx];
+            positiveIdx++;
+        }
+        return ans;
     }
 }
