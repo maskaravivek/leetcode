5f781fadd5385130cc837d5032f1fea450e2194d
Vivek Maskara <maskaravivek@gmail.com>
2020-05-25 18:29:18 -0700

    With move zeros

---
+++ MoveZeros.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/move-zeroes/
+
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[idx++] = nums[i];
+            }
+        }
+        for(int i = idx; i< nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
