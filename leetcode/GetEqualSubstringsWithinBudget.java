cd5a17cd67c4c5687e16618a3ee7c08b47d1cc0d
Vivek Maskara <maskaravivek@gmail.com>
2020-06-01 22:12:37 -0700

    with get equal substring withing budget

---
+++ leetcode/GetEqualSubstringsWithinBudget.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+class GetEqualSubstringsWithinBudget {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int cost[] = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            cost[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        }
+
+        int currentCost = 0;
+        int left = 0;
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < cost.length; i++) {
+            currentCost += cost[i];
+
+            while (currentCost > maxCost) {
+                currentCost -= cost[left++];
+            }
+
+            res = Math.max(res, i - left + 1);
+        }
+        return res;
+    }
+}
