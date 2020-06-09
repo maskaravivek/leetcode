fae1849ad840d0f159bb64718f0ef06a753cd1a2
Vivek Maskara <maskaravivek@gmail.com>
2020-06-08 17:59:13 -0700

    Max product of three numbers

---
+++ leetcode/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/maximum-product-of-three-numbers/
+
+package leetcode;
+
+import java.util.Arrays;
+
+public class MaximumProductOfThreeNumbers {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[nums.length - 1],
+                nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3]);
+    }
+}
