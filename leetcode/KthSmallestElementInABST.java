b40a146579eb184a8538229dd61cbdee9d32f750
Vivek Maskara <maskaravivek@gmail.com>
2020-06-09 17:13:23 -0700

    With kth smallest element in a bst

---
+++ leetcode/KthSmallestElementInABST.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class KthSmallestElementInABST {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> tree = new ArrayList();
+        inOrder(root, tree);
+        return tree.get(k - 1);
+    }
+
+    public void inOrder(TreeNode root, List<Integer> tree) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left, tree);
+        tree.add(root.val);
+        inOrder(root.right, tree);
+    }
+}
