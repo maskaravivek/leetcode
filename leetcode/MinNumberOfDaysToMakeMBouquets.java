24cc8f9ab2de7a7290da97d6a6b67d093ee92efd
Vivek Maskara <maskaravivek@gmail.com>
2020-06-15 19:14:46 -0700

    with min number of bouquets

--- leetcode/MinNumberOfDaysToMakeMBouquets.java
+++ leetcode/MinNumberOfDaysToMakeMBouquets.java
@@ -1,81 +1,45 @@
+// https://leetcode.com/problems/minimum-number-of-days-to-make-m-bouquets
 package leetcode;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
 public class MinNumberOfDaysToMakeMBouquets {
     public int minDays(int[] bloomDay, int m, int k) {
         if (bloomDay.length < m * k) {
             return -1;
         }
-        int arr[] = new int[bloomDay.length - k + 1];
 
-        int idx = 0;
-        Deque<Integer> queue = new LinkedList<>();
+        int min = 0, max = 0;
+
         for (int i = 0; i < bloomDay.length; i++) {
-
-            while (!queue.isEmpty() && queue.peek() < i - k + 1) {
-                queue.poll();
-            }
-
-            while (!queue.isEmpty() && bloomDay[queue.peekLast()] < bloomDay[i]) {
-                queue.removeLast();
-            }
-
-            queue.addLast(i);
-
-            if (i >= k - 1) {
-                arr[idx++] = bloomDay[queue.peekFirst()];
-            }
+            min = Math.min(min, bloomDay[i]);
+            max = Math.max(max, bloomDay[i]);
         }
 
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < arr.length; i++) {
-            map.put(i, arr[i]);
-        }
-
-        List<Map.Entry<Integer, Integer>> list = new LinkedList<Map.Entry<Integer, Integer>>(map.entrySet());
-
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                return (o1.getValue()).compareTo(o2.getValue());
-            }
-        });
-
-        HashMap<Integer, Integer> temp = new LinkedHashMap<Integer, Integer>();
-        for (Map.Entry<Integer, Integer> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
-        }
-
-        System.out.println(Arrays.toString(map.entrySet().toArray()));
-
-        ArrayList<Integer> noList = new ArrayList();
-
-        int wait = Integer.MIN_VALUE;
-        int cnt = 0;
-        for (Integer key : temp.keySet()) {
-            if (noList.contains(key)) {
-                continue;
-            }
-
-            wait = Math.max(wait, temp.get(key));
-            cnt++;
-            if (cnt == m) {
-                break;
-            }
-            for (int i = key - k + 1; i < key + k; i++) {
-                noList.add(i);
+        while (min < max) {
+            int mid = (min + max) / 2;
+            int bouquets = getBouquets(bloomDay, mid, k);
+            if (bouquets < m) {
+                min = mid + 1;
+            } else {
+                max = mid;
             }
         }
-        return cnt == m ? wait : -1;
+        return min;
+    }
+
+    public int getBouquets(int[] bloomDay, int day, int k) {
+        int flowers = 0, bouquets = 0;
+
+        for (int i = 0; i < bloomDay.length; i++) {
+            if (bloomDay[i] <= day) {
+                flowers++;
+            } else {
+                flowers = 0;
+            }
+            if (flowers == k) {
+                bouquets++;
+                flowers = 0;
+            }
+        }
+        return bouquets;
     }
 }
