69118207b95d5cdb719840411f9c0919b51c6061
Vivek Maskara <maskaravivek@gmail.com>
2020-08-22 00:09:58 -0700

    letter tile possibilities

--- leetcode/backtracking/LetterTilePossibilities.java
+++ leetcode/backtracking/LetterTilePossibilities.java
@@ -1,19 +1,36 @@
+https://leetcode.com/problems/letter-tile-possibilities/
+
 package leetcode.backtracking;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class LetterTilePossibilities {
-    public int numTilePossibilities(String tiles) {
-        Set<String> set = new HashSet<String>();
-        backtracking(tiles, 0, set);
-        return set.size();
+    static int count = 0;
+
+    public static void main(final String[] args) throws java.lang.Exception {
+        System.out.println(numTilePossibilities("AAB"));
     }
 
-    public void backtracking(String tiles, int position, Set<String> set) {
+    public static int numTilePossibilities(String tiles) {
+        char arr[] = tiles.toCharArray();
+        Arrays.sort(arr);
+        permute(arr, new boolean[arr.length]);
+        return count - 1;
+    }
 
-        for (int i = position; i < tiles.length(); i++) {
-            
+    public static void permute(char[] tiles, boolean[] used) {
+        count++;
+        for (int i = 0; i < tiles.length; i++) {
+            if (used[i] || (i > 0 && tiles[i] == tiles[i - 1] && !used[i - 1])) {
+                continue;
+            }
+
+            used[i] = true;
+            permute(tiles, used);
+            used[i] = false;
         }
     }
 }
