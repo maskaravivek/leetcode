7e0cbd5af0de8e323420b68b7e3b6dc07045870c
Vivek Maskara <maskaravivek@gmail.com>
2020-08-29 23:31:23 -0700

    Max length of positive product

---
+++ leetcode/DetectPatternOfLengthM.java
@@ -0,0 +1,47 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DetectPatternOfLengthM {
+
+    public static void main(final String[] args) throws java.lang.Exception {
+        System.out.println(containsPattern(new int[] { 1, 2, 1, 2, 1, 1, 1, 3 }, 2, 2));
+    }
+
+    public static boolean containsPattern(int[] arr, int m, int k) {
+        for (int i = 0; i <= arr.length - m; i++) {
+            if (pattern(arr, m, k, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean pattern(int arr[], int m, int k, int start) {
+        int[] pattern = new int[m];
+        String lastPattern = "";
+        int count = 0;
+        for (int i = start; i <= arr.length - m; i += m) {
+            int idx = 0;
+            while (idx < m) {
+                pattern[idx] = arr[idx + i];
+                idx++;
+            }
+
+            String key = Arrays.toString(pattern);
+            if (lastPattern.equals(key)) {
+                count++;
+            } else {
+                lastPattern = key;
+                count = 1;
+            }
+            if (count >= k) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ leetcode/MaxLengthSubArrayWithPositiveProduct.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product
+
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MaxLengthSubArrayWithPositiveProduct {
+    public int getMaxLen(int[] nums) {
+        int max = 0;
+        int firstNegativeIndex = -1;
+        int zeroIndex = -1;
+        int negativeCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                negativeCount++;
+                if (firstNegativeIndex == -1) {
+                    firstNegativeIndex = i;
+                }
+            }
+
+            if (nums[i] == 0) {
+                negativeCount = 0;
+                firstNegativeIndex = -1;
+                zeroIndex = i;
+            } else {
+                if (negativeCount % 2 == 0) {
+                    max = Math.max(i - zeroIndex, max);
+                } else {
+                    max = Math.max(i - firstNegativeIndex, max);
+                }
+            }
+        }
+        return max;
+    }
+}
