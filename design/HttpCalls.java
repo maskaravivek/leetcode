6852953de8fad30914b27b3c10382123c45bfd0f
Vivek Maskara <maskaravivek@gmail.com>
2020-09-16 01:10:39 -0700

    Http get

---
+++ design/HttpCalls.java
@@ -0,0 +1,74 @@
+package design;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpCalls {
+
+    public static void main(String[] args) throws IOException {
+        Map<String, String> params = new HashMap<>();
+        params.put("hello", "world");
+        params.put("test", "value");
+        htttpGet("https://jsonplaceholder.typicode.com/todos/1", params);
+    }
+
+    static class ParameterStringBuilder {
+        public static String getParamsString(Map<String, String> params) throws UnsupportedEncodingException {
+            StringBuilder result = new StringBuilder();
+
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+                result.append("=");
+                result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+                result.append("&");
+            }
+
+            String resultString = result.toString();
+            return resultString.length() > 0 ? resultString.substring(0, resultString.length() - 1) : resultString;
+        }
+    }
+
+    public static String htttpGet(String urlString, Map<String, String> params) throws IOException {
+        String paramString = ParameterStringBuilder.getParamsString(params);
+        URL url = new URL(urlString + "?" + paramString);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setConnectTimeout(5000);
+        con.setReadTimeout(5000);
+
+        int status = con.getResponseCode();
+
+        System.out.println(con.getURL());
+        System.out.println("Status code: " + status);
+
+        Reader reader;
+        if (status > 299) {
+            reader = new InputStreamReader(con.getErrorStream());
+        } else {
+            reader = new InputStreamReader(con.getInputStream());
+        }
+
+        BufferedReader in = new BufferedReader(reader);
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+
+        con.disconnect();
+
+        System.out.println(content.toString());
+        return content.toString();
+    }
+}
